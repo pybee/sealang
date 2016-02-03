3d3864024c28c71802be581f1e4c9e4fbe12dd02
Russell Keith-Magee <russell@keith-magee.com>
2016-02-03 22:03:28 +0800

    Tweaks to package registration.

--- setup.py
+++ setup.py
@@ -19,31 +19,35 @@ try:
         llvm_ldflags = [p.decode('utf-8') for p in llvm_ldflags]
 
     setup(
-        name="sealang",
-        version="3.9.dev258341",
-        description="An extended set of Python bindings for libclang",
-        long_description=open("README.rst").read(),
-        url="http://github.com/pybee/sealang",
-        license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+        name='sealang',
+        version='3.9.dev258341',
+        description='An extended set of Python bindings for libclang',
+        long_description=open('README.rst').read(),
+        url='http://github.com/pybee/sealang',
+        license='License :: OSI Approved :: University of Illinois/NCSA Open Source License',
         classifiers=[
-            "Intended Audience :: Developers",
-            "License :: OSI Approved ::  University of Illinois/NCSA Open Source License",
-            "Programming Language :: Python",
-            "Development Status :: 5 - Production/Stable",
-            "Topic :: Software Development :: Compilers"
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+            'Programming Language :: Python',
+            'Development Status :: 5 - Production/Stable',
+            'Topic :: Software Development :: Compilers',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
         ],
-        keywords=["llvm", "clang", "libclang"],
-        author="LLVM team and Russell Keith-Magee",
-        packages=["clang"],
+        keywords=['llvm', 'clang', 'libclang'],
+        author='LLVM team and Russell Keith-Magee',
+        author_email='russell@keith-magee.com',
+        packages=[
+            'clang',
+            'sealang'
+        ],
         ext_modules=[
             Extension(
-                "sealang",
-                sources=["sealang/sealang.cpp"],
+                'sealang',
+                sources=['sealang/sealang.cpp'],
                 libraries=[
                     'clangAST', 'clangBasic', 'clangLex', 'LLVMSupport',
                     'm', 'z', 'pthread', 'curses'
@@ -54,7 +58,7 @@ try:
         ],
         # if use nose.collector, many plugins not is avaliable
         # see: http://nose.readthedocs.org/en/latest/setuptools_integration.html
-        test_suite="nose.collector",
+        test_suite='nose.collector',
         tests_require=['nose']
     )
 
